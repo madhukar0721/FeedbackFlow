bc867b31f755bc008cf7b32cecadaa2e0da63e60
Madhukar Kumar <madhukarkumarsocial@gmail.com>
2024-09-23 22:09:59 +0530

    Update layout.tsx

--- feedbackflow-dashboard/src/app/layout.tsx
+++ feedbackflow-dashboard/src/app/layout.tsx
@@ -13,7 +13,7 @@ export default function RootLayout({
       <html lang="en">
         <body>
           <Navbar />
-          {children} 
+          {children}
         </body>
       </html>
     </ClerkProvider>
