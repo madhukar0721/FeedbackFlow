33c908b4839b32bd004b989e8c6004132495f6cd
Madhukar <madhukarkumarsocial@gmail.com>
2024-11-22 07:27:06 +0530

    fixed instructions page

--- feedbackflow-dashboard/src/app/(user)/projects/[projectId]/instructions/page.tsx
+++ feedbackflow-dashboard/src/app/(user)/projects/[projectId]/instructions/page.tsx
@@ -11,33 +11,46 @@ const Page = ({
 }) => {
   if (!params.projectId) return <div> Invalid Project ID</div>;
   if (!process.env.WIDGET_URL) return <div>Missing WIDGET_URL</div>;
-  
 
   return (
     <div>
-       <div>
-        <Link href={`/projects/${params.projectId}`} className="flex items-center text-zinc-800 mb-5 w-fit"><ChevronLeft className="h-5 w-5 mr-1" />Back</Link>
+      <div>
+        <Link
+          href={`/projects/${params.projectId}`}
+          className="flex items-center text-zinc-800 mb-5 w-fit"
+        >
+          <ChevronLeft className="h-5 w-5 mr-1" />
+          Back
+        </Link>
       </div>
       <h1 className="text-xl font-bold mb-2">Start Collecting Feedback</h1>
-      <p className="lext-lg text-secondary-foreground">Embed the code in your site</p>
+      <p className="lext-lg text-secondary-foreground">
+        Embed the code in your site
+      </p>
       <div className="bg-blue-950 p-4 my-5 rounded-md relative">
-      <CopyBtn text={`<my-widget project-id="${params .projectId}"></my-widget>\n<script src="${process.env.WIDGET_URL}/widget.umd.js"></script>`} />
+        <CopyBtn
+          text={`<my-widget project-id="${params.projectId}"></my-widget>\n<script src="${process.env.WIDGET_URL}/widget.umd.js"></script>`}
+        />
         <code className="text-white">
           {`<my-widget project-id="${params.projectId}"></my-widget>`}
           <br />
           {`<script src="${process.env.WIDGET_URL}/widget.umd.js"></script>`}
         </code>
-        
-        
-        
       </div>
-      <div className="flex  item-center gap-4">
-      <p>Check out this demo example</p>
 
-      <Link  target="_blank" href={"https://github.com/madhukar0721/FeedbackFlow/blob/main/demo_example.html"}><MoveUpRight /></Link>
-      
-
-      </div>
+      <Link
+        target="_blank"
+        href={
+          "https://github.com/madhukar0721/FeedbackFlow/blob/main/demo_example.html"
+        }
+      >
+        <div className="flex  item-center gap-3">
+          <p>Check out this demo example</p>
+          
+            <MoveUpRight className="text-red-800 border rounded" />
+          
+        </div>
+      </Link>
     </div>
   );
 };
