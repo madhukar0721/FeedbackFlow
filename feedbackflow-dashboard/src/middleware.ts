a93ac35b8a5cbc5209ef62239ff4faa0bf2b93c3
Madhukar <madhukarkumarsocial@gmail.com>
2024-11-22 05:43:39 +0530

    dockerrisation

--- feedbackflow-dashboard/src/middleware.ts
+++ feedbackflow-dashboard/src/middleware.ts
@@ -1,24 +1,21 @@
-import { clerkMiddleware , createRouteMatcher } from "@clerk/nextjs/server";
 
+import { clerkMiddleware, createRouteMatcher } from "@clerk/nextjs/server";
 
-
-const isProctectedRoute = createRouteMatcher([
+const isProtectedRoute = createRouteMatcher([
   "/dashboard(.*)",
   "/projects(.*)",
 ]);
 
 export default clerkMiddleware((auth, req) => {
-  if (isProctectedRoute(req)) {
+  if (isProtectedRoute(req)) {
     auth().protect();
   }
 });
 
-
-
 export const config = {
   matcher: [
-    // Skip Next.js internals and all static files, unless found in search params
-    '/((?!_next|[^?]*\\.(?:html?|css|js(?!on)|jpe?g|webp|png|gif|svg|ttf|woff2?|ico|csv|docx?|xlsx?|zip|webmanifest)).*)',
+    // Match all paths except for _next/image, _next/static, favicon.ico, etc.
+    '/((?!_next/image|_next/static|favicon.ico).*)',
     // Always run for API routes
     '/(api|trpc)(.*)',
   ],
